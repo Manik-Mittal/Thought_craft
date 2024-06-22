3eac1f730422ad8683c59dc746e6256d69da12fe
Manik-Mittal <131521271+Manik-Mittal@users.noreply.github.com>
2024-06-22 18:41:24 +0530

    establishes connection to database

--- server/server.js
+++ server/server.js
@@ -2,19 +2,27 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const dotenv = require('dotenv');
+const connectDb = require('../config/db');
 const app = express();
-
+//const userRoutes = require('./routes/userRoutes')
 
 //env config
 dotenv.config()
 
+//mongo connect 
+connectDb();
+//*********************** */
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
 
+//********************************************* */
+//routes
+//  app.use('/api/v1/user', userRoutes)
+
 
 const PORT = process.env.PORT || 8080
-
+//routes
 
 app.get('/', (req, res) => {
     res.status(200).send({
